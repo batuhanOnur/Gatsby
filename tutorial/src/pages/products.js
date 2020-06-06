a3de4d2da322291281805d2167dde4cde368465b
batuhan <batuhanonur29@gmail.com>
2020-06-06 15:13:22 +0300

    basics

---
+++ tutorial/src/pages/products.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Layout from '../components/layout';
+
+
+export default class products extends Component {
+    render() {
+        return (
+            <Layout>
+                <h1>this is product page</h1>
+            </Layout>
+        )
+    }
+}
